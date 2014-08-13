fd0783f978bcdf390d8c2a22cf2a71c204406460
apeckham <apeckham@gmail.com>
2014-08-12 21:51:25 -0700

    remove sleep

--- runner.sh
+++ runner.sh
@@ -1,3 +1,2 @@
 erb config/config.yml.erb >config/config.yml
 bundle exec bin/snowplow-emr-etl-runner --enrichments config/enrichments --config config/config.yml --skip ${RUNNER_SKIP:-shred}
-sleep 21600
