3237484eaa867ffe9cc6dce861fe30791e082d16
apeckham <apeckham@gmail.com>
2014-08-13 11:38:29 -0700

    run once a day in a loop

--- runner.sh
+++ runner.sh
@@ -1,2 +1,7 @@
 erb config/config.yml.erb >config/config.yml
-bundle exec bin/snowplow-emr-etl-runner --enrichments config/enrichments --config config/config.yml --skip ${RUNNER_SKIP:-shred}
+
+while :
+do
+  bundle exec bin/snowplow-emr-etl-runner --enrichments config/enrichments --config config/config.yml --skip ${RUNNER_SKIP:-shred}
+  sleep 86400
+done
