ff60eaa9fba1d3a232b384d13f0fe7c9522a15e6
Dave Newman <dave@snappyco.de>
2015-03-24 15:32:55 -0700

    run on schedule rather than sleep

--- runner.sh
+++ runner.sh
@@ -1,7 +1,2 @@
 erb config/config.yml.erb >config/config.yml
-
-while :
-do
-  bundle exec bin/snowplow-emr-etl-runner --enrichments config/enrichments --config config/config.yml --skip ${RUNNER_SKIP:-shred}
-  sleep ${SLEEP:-86400}
-done
+bundle exec bin/snowplow-emr-etl-runner --enrichments config/enrichments --config config/config.yml --skip ${RUNNER_SKIP:-shred}
